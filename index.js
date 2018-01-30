7954c55a370e802edeb0a54cc74a5513a27444f6
An Nuyen <annuyen@gmail.com>
2018-01-30 19:01:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const logDriversbyHometown = function(drivers, location) {
     drivers.forEach(function(driver) {
       if (driver.hometown === location) {
         console.log(driver.name)
-      }
-    })
+      };
+    });
 };
 
 const driversByRevenue = function(drivers) {
